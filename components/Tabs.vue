bc23e31ec81d7656c11e93c7b9c1bd2c2ccdf086
Abdellah Chadidi <chadidi97@gmail.com>
2019-03-16 12:37:31 +0100

    Add Tabs component

---
+++ components/Tabs.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-tabs
+    slot="extension"
+    centered
+    color="red"
+    slider-color="white"
+  >
+    <v-tab
+      v-for="(tab, key) in tabs"
+      :key="key"
+    >
+      {{ tab }}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  props: { tabs: { type: Array, default: Array } }
+}
+</script>
+
+<style>
+</style>
