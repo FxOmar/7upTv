e8c64bf7c19f829543f79aac72d2d5166288b5e8
Abdellah Chadidi <chadidi97@gmail.com>
2019-08-03 17:28:13 +0100

    Add block component

--- components/Block.vue
+++ components/Block.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="block">
     <div class="block-head">
-      <h1 class="text-xs-left">
+      <h1 class="text-xs-left ml-4">
         {{ title }}
       </h1>
     </div>
     <div class="block-content">
-      <Movie
-        v-for="(card, index) in data"
-        :key="index"
-        :thumbnail="card.thumbnail"
-        :title="card.title"
-        :views="card.views"
-        :time="card.time"
-      />
+      <v-container>
+        <v-layout
+          row
+          wrap
+        >
+          <Movie
+            v-for="(card, index) in data"
+            :key="index"
+            :thumbnail="card.thumbnail"
+            :title="card.title"
+            :views="card.views"
+            :time="card.time"
+          />
+          <v-flex
+            xs12
+            sm6
+            md4
+            lg3
+          >
+            <v-card
+              class="text-xs-center ma-2"
+              to="/movies"
+            >
+              <v-responsive class="pa-5">
+                <v-icon large>
+                  add
+                </v-icon>
+              </v-responsive>
+              <v-card-text>
+                <div class="subheading">
+                  more
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </div>
   </div>
 </template>
@@ -33,6 +62,6 @@ export default {
 
 <style>
 .block {
-  margin-bottom: 150px;
+  margin-bottom: 40px;
 }
 </style>
