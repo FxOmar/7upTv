8747fc4bdf269797f19153f31d52fccb832c3250
Omar chadidi <omarchadidi@gmail.com>
2019-11-18 19:43:38 +0100

    working on the register page.

--- pages/register.vue
+++ pages/register.vue
@@ -1,87 +1,99 @@
 <template>
-<div class="container mx-auto py-8">
-    <div class="flex justify-center items-center">
-    	<form class="w-full max-w-lg">
-    		<div class="flex flex-wrap -mx-3 mb-6">
-    			<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-    				<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-    					for="grid-first-name">
-    					First Name
-    				</label>
-    				<input
-    					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-    					id="grid-first-name" type="text" placeholder="Jane">
-    				<p class="text-red-500 text-xs italic">Please fill out this field.</p>
-    			</div>
-    			<div class="w-full md:w-1/2 px-3">
-    				<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-    					for="grid-last-name">
-    					Last Name
-    				</label>
-    				<input
-    					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-    					id="grid-last-name" type="text" placeholder="Doe">
-    			</div>
-    		</div>
-    		<div class="flex flex-wrap -mx-3 mb-6">
-                <div class="w-full px-3 py-2">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                        for="grid-password">
-                        Email
-                    </label>
-                    <input
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="grid-password" type="email" placeholder="Jane.doe123@email.com">
-                </div>
-                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-    				<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-    					for="grid-password">
-    					Password
-    				</label>
-    				<input
-    					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-    					id="grid-first-name" type="password" placeholder="**********">
-    				<p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
-    			</div>
-    			<div class="w-full md:w-1/2 px-3">
-    				<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-    					for="grid-password">
-    					Confirm Password
-    				</label>
-    				<input
-    					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-    					id="grid-last-name" type="password" placeholder="*********">
-    			</div>
-                
-    		</div>
-            <div class="flex flex-wrap -mx-3 mb-6">
-                <div class="mx-3 mb-6 w-full">
-                	<div class="md:w-1/3"></div>
-                	<label class="md:w-2/3 block text-gray-500 font-bold">
-                		<input class="mr-2 leading-tight" type="checkbox">
-                		<span class="text-sm">
-                			Send me your newsletter!
-                		</span>
-                	</label>
-                </div>
-                <div class="w-full px-3 py-3">
-                    <app-button class="w-full font-bold"
-                        color="blue">
-                        {{ $t('New account') }}
-                    </app-button>
-                </div>
-            </div>
-    	</form>
-    </div>
-</div>
+	<div class="container mx-auto px-3 py-5">
+		<div class="flex justify-center">
+			<div class="lg:w-1/3 w-full">
+				<div class="shadow rounded bg-white mt-4">
+					<div class="px-5 py-3 border-b">
+						<div class="text-xl font-bold text-gray-500">
+							{{ $t('Register') }}
+						</div>
+					</div>
+					<div class="p-5">
+						<form  method="post" @submit.prevent="register">
+							<div class="-mx-3 mb-6">
+								<app-text-input class="mb-3">
+									<input name="name" v-model="name" :placeholder="$t('Username')" type="text" required>
+									<div v-if="error.name" slot="helper">{{ error.name[0] }}</div>
+								</app-text-input>
+							</div>
+							<div class="-mx-3 mb-6">
+								<app-text-input class="mb-3">
+									<input name="email" v-model="email" :placeholder="$t('Email')" type="text" required>
+									<div v-if="error.email" slot="helper">{{ error.email[0] }}</div>
+								</app-text-input>
+							</div>
+							<div class="-mx-3 mb-6">
+								<app-text-input class="mb-3">
+									<input name="password" v-model="password" :placeholder="$t('password')" type="password" required>
+									<div v-if="error.password" slot="helper">{{ error.password[0] }}</div>
+								</app-text-input>
+								<app-text-input class="mb-3">
+									<input name="password_confirmation" v-model="password_confirmation" :placeholder="$t('Confirm password')" type="password" required>
+									<div v-if="password != password_confirmation" slot="helper">password is not the same as above!</div>
+								</app-text-input>
+							</div>
+							<div class="-mx-3 mb-6">
+								<div class="pb-3">
+									<label class="md:w-2/3 block text-gray-500 font-bold">
+										<input class="mr-2 leading-tight" type="checkbox">
+										<span class="text-sm">
+											Send me your newsletter!
+										</span>
+									</label>
+								</div>
+								<app-button class="w-full font-bold" color="blue">
+									{{ $t('New account') }}
+								</app-button>
+							</div>
+						</form>
+						<div class="has-text-centered" style="margin-top: 20px">
+							Already got an account? <nuxt-link to="/login">Login</nuxt-link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
 
+export default {
+  middleware:['guest'],
+  components: {
+  },
+
+  data() {
+    return {
+      name: '',
+      email: '',
+	  password: '',
+	  password_confirmation: '',
+	  error: {}
+    }
+  },
+
+  methods: {
+    async register() {
+      try {
+        await this.$axios.post('/users/register', {
+          name: this.name,
+          email: this.email,
+		  password: this.password,
+		  password_confirmation: this.password
+        })
+
+        this.$router.push('/login')
+      } catch (e) {
+		this.error = e.response.data.errors
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
+.input{
+	@apply border
+}
 </style>
--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Hi, User</h1>
+  <section class="section">
+    <div class="container">
+      <h2 class="title">My Profile</h2>
+      <div class="content">
+        <p>
+          <strong>Username:</strong>
+           {{ loggedInUser.result.name }}
+        </p>
+        <p>
+          <strong>Email:</strong>
+          {{ loggedInUser.result.email }}
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  middleware:['auth'],
+  computed: {
+    ...mapGetters(['loggedInUser'])
+  }
+}
+</script>
